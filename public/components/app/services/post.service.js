7baffb1575837ab7bde921d5e0868c86aad698fe
Chase Scheu <chasescheu@gmail.com>
2017-02-23 12:07:04 -0700

    api connect test7

--- public/components/app/services/post.service.js
+++ public/components/app/services/post.service.js
@@ -1,6 +1,6 @@
 myApp.factory('post_factory', ['$resource',
     function ($resource) {
-        return $resource("52.34.173.43//api/posts/:pk/", {},{
+        return $resource("https://52.34.173.43//api/posts/:pk/", {},{
             query: { method: "GET", isArray: true },
             create: { method: "POST"},
             get: { method: "GET"},
