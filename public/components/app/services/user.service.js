839328db269260f490b7a150f818c1baf1fe4cbb
Chase Scheu <chasescheu@gmail.com>
2017-02-23 14:16:04 -0700

    api connect test8

--- public/components/app/services/user.service.js
+++ public/components/app/services/user.service.js
@@ -1,6 +1,6 @@
 myApp.factory('user_factory', ['$resource',
     function ($resource) {
-        return $resource("//52.34.173.43//api/users/:pk/", {},{
+        return $resource("52.34.173.43//api/users/:pk/", {},{
             query: { method: "GET", isArray: true },
             create: { method: "POST"},
             get: { method: "GET"},
