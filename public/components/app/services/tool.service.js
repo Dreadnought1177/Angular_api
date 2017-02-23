fe5b139e389b488dcf44898385be880d4d687b72
Chase Scheu <chasescheu@gmail.com>
2017-02-23 14:33:22 -0700

    api connect test8

--- public/components/app/services/tool.service.js
+++ public/components/app/services/tool.service.js
@@ -1,6 +1,6 @@
 myApp.factory('tool_factory', ['$resource',
     function ($resource) {
-        return $resource("//52.34.173.43//api/tools/:pk/", {},{
+        return $resource("http://52.34.173.43//api/tools/:pk/", {},{
             query: { method: "GET", isArray: true },
             create: { method: "POST"},
             get: { method: "GET"},
