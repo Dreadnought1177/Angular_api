920c687bfc48002abda96403cf6e7170d7614908
Chase Scheu <chasescheu@gmail.com>
2017-02-23 11:30:49 -0700

    api connect test4

--- public/components/app/services/comments.service.js
+++ public/components/app/services/comments.service.js
@@ -1,6 +1,6 @@
 myApp.factory('comment_factory', ['$resource',
     function ($resource) {
-        return $resource("https://52.34.173.43//api/comments/:pk/", {},{
+        return $resource("http://52.34.173.43//api/comments/:pk/", {},{
             query: { method: "GET", isArray: true },
             create: { method: "POST"},
             get: { method: "GET"},
