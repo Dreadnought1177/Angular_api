7a05b44b9105825362e47839e18edf039ae5afa3
Chase Scheu <chasescheu@gmail.com>
2017-02-23 17:26:21 -0700

    firsty first

--- public/components/app/services/tool.service.js
+++ public/components/app/services/tool.service.js
@@ -1,6 +1,6 @@
 myApp.factory('tool_factory', ['$resource',
     function ($resource) {
-        return $resource("https://afternoon-plateau-31075.herokuapp.com:443/52.34.173.43//api/tools/:pk/", {},{
+        return $resource("//52.34.173.43//api/tools/:pk/", {},{
             query: { method: "GET", isArray: true },
             create: { method: "POST"},
             get: { method: "GET"},
